ee63f0a9f746e81f73597f3f10fff9c43af7ec32
murat <murattopuzresmi@yandex.com>
2019-07-13 22:28:56 +0000

    Al ali

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 Route::apiResource('users', 'UserController')->middleware('auth:api');
+Route::post('/user/login', 'LoginController@login')->middleware('guest');
+Route::post('/user/register', 'LoginController@register')->middleware('guest');
