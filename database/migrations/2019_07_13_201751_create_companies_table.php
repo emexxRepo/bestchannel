15eba4647babbab226edc17d02f130e17543bcc7
murat <murattopuzresmi@yandex.com>
2019-07-13 20:30:31 +0000

    three committ

---
+++ database/migrations/2019_07_13_201751_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('photo');
+            $table->string('email');
+            $table->string('password');
+            $table->unsignedInteger('sector_id')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ database/migrations/2019_07_13_202523_create_opportunities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpportunitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opportunities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('company_id')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('title');
+            $table->text('content');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opportunities');
+    }
+}
